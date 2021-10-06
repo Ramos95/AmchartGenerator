d5792e1b60b509b3fa62adbc2512b5c63704ec1f
Estuardo <eramos.9588@gmail.com>
2021-10-06 11:14:07 -0600

    applying format to code

--- src/App.js
+++ src/App.js
@@ -10,7 +10,17 @@ export default function App() {
         chartID="chart"
         chartData={[
           { category: "2020-01-01", value: 100, valuex: 300 },
-          { category: "2020-02-01", value: 200, valuex: 400 }
+          { category: "2020-02-01", value: 200, valuex: 400 },
+          { category: "2020-03-01", value: 100, valuex: 300 },
+          { category: "2020-04-01", value: 200, valuex: 400 },
+          { category: "2020-05-01", value: 100, valuex: 300 },
+          { category: "2020-06-01", value: 200, valuex: 400 },
+          { category: "2020-07-01", value: 100, valuex: 300 },
+          { category: "2020-08-01", value: 200, valuex: 400 },
+          { category: "2020-09-01", value: 100, valuex: 300 },
+          { category: "2020-10-01", value: 200, valuex: 400 },
+          { category: "2020-11-01", value: 100, valuex: 300 },
+          { category: "2020-12-01", value: 200, valuex: 400 },
         ]}
         series={["value", "valuex"]}
         chartType="column"

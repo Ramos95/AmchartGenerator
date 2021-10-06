3f4fbf9f8b009c217391aae671bc76e01c93a29d
Estuardo <eramos.9588@gmail.com>
2021-10-06 11:17:17 -0600

    adding types:
    adding const CHART_OPTIONS to handle the type of charts to use

---
+++ src/ChartTypes.js
@@ -0,0 +1,7 @@
+import { XYChart, PieChart } from "@amcharts/amcharts4/charts";
+const CHART_OPTIONS = [
+  { name: "Column Chart", type: XYChart },
+  { name: "Line Chart", type: XYChart },
+  { name: "Pie Chart", type: PieChart },
+];
+export default CHART_OPTIONS;

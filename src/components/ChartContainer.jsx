9bdd2129691c9af6ae719ece30d1f162e319a1e1
Estuardo <eramos.9588@gmail.com>
2021-10-06 11:21:16 -0600

    refactor code: implement select component
    implementing select using CHART_OPTIONS as it options prop value
     component and adding a selectedType state to handle when selection changes,
    also refactoring the switch to support the values on CHART_OPTIONS

--- src/components/ChartContainer.jsx
+++ src/components/ChartContainer.jsx
@@ -1,54 +1,56 @@
-import React, { useRef, useLayoutEffect } from "react";
+import React, { useRef, useLayoutEffect, useState } from "react";
 import * as am4core from "@amcharts/amcharts4/core";
 import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 import ColumnChart from "../charts/XYChart";
 import CircleChart from "../charts/CircleChart";
 import LineChart from "../charts/LineChart";
+import Select from "./Select";
+import CHART_OPTIONS from "../ChartTypes";
 
 export default function ChartContainer({
   chartData,
   chartType,
   chartID,
-  series
+  series,
 }) {
+  const [selectedType, setSelectedType] = useState(CHART_OPTIONS[0]);
   const chartComponent = useRef(null);
 
   useLayoutEffect(() => {
     let chart = createChart();
-    chartComponent.current = chart
-    
+    chartComponent.current = chart;
+
     return () => {
-      chart.dispose();
+      chart.newChart.dispose();
     };
-  }, [chartData]);
+  }, [chartData, selectedType]);
 
   function createChart() {
     am4core.useTheme(am4themes_animated);
-    
-    switch (chartType) {
-      case "column":
-        console.log("lol")
-       return new ColumnChart(
+
+    switch (selectedType.name) {
+      case "Column Chart":
+        return new ColumnChart(
           chartData,
-          "XYChart",
+          selectedType.type,
           series,
           chartID,
           "date"
         );
 
-      case "line":
-       return new LineChart(
+      case "Line Chart":
+        return new LineChart(
           chartData,
-          "XYChart",
+          selectedType.type,
           series,
           chartID,
           "date"
         );
 
-      case "donut":
-       return new CircleChart(
+      case "Pie Chart":
+        return new CircleChart(
           chartData,
-          "DonutChart",
+          selectedType.type,
           series,
           chartID,
           "date"
@@ -60,8 +62,17 @@ export default function ChartContainer({
 
     /* chart.data = chartData;
     chart.scrollbarX = new am4core.Scrollbar(); */
-   
   }
 
-  return <div id={chartID}></div>;
+  return (
+    <div className="chart-wrapper">
+      <Select
+        onChange={setSelectedType}
+        options={CHART_OPTIONS}
+        optionLabel={"name"}
+        defaultValue={"Column Chart"}
+      />
+      <div id={chartID} className="chart-container"></div>
+    </div>
+  );
 }
